62fc02c0ea1b61605c00b4c0411537bc8f85cb5b
Brandon-the-Lecturer <89731991+Brandon-the-Lecturer@users.noreply.github.com>
2024-10-16 22:30:47 +0000

    feat: add data product level 3

--- src/logic/pbp.py
+++ src/logic/pbp.py
@@ -24,8 +24,7 @@ def _add_possession_info_per_drive(
     
     mask = (data['Quarter'] == drive_quarter) & \
     (data['Time'] <= drive_start_time) & \
-    (data['Time'] > drive_end_time) & \
-    (data['EPB'] != data['EPA'])
+    (data['Time'] > drive_end_time)
 
     data.loc[mask, "Possession"] = team
     return data
@@ -47,7 +46,21 @@ def _add_possession_info(df2: pd.DataFrame, df1:pd.DataFrame ) -> pd.DataFrame:
 
             df2 = _add_possession_info_per_drive(df2, drive_quarter+1, pd.Timedelta(minutes=15), drive_end_time+pd.Timedelta(15), drive_possession_team)
 
+    df2.loc[(df2['EPB'] == 0) & (df2['EPA'] == 0), 'Possession'] = df2['Possession'].shift()
     return df2
+
+
+def _add_drive_counter(data:pd.DataFrame) -> pd.DataFrame:
+    data['Drive_No'] = (data['Possession'] != data['Possession'].shift()).cumsum()
+    data['Drive_No'] = data['Drive_No'].astype('int')
+    return data
+
+def _fill_with_max_until_max_reached(group):
+    max_value = group['EPB'].max()
+    max_index = group['EPB'].idxmax()
+    # Werte bis zum Maximum kopieren, danach mit dem Maximalwert auffüllen
+    group['xP'] = group['EPB'].where(group.index <= max_index, max_value)
+    return group
 
 def get_pbp_data(season: int, week: int, game: str):
     pbp = DataService_PBP.get_pbp_data(season, week, game)
@@ -58,8 +71,12 @@ def get_pbp_data(season: int, week: int, game: str):
 
     pbp = _add_possession_info(pbp, drive_visitors)
     pbp = _add_possession_info(pbp, drive_home)
-
+    pbp = _add_drive_counter(pbp)
     return pbp
+
+def get_pbp_data_with_xp(season: int, week: int, game: str):
+    pbp = get_pbp_data(season, week, game)
+    return pbp.groupby('Drive_No', group_keys=False).apply(_fill_with_max_until_max_reached)
 
 def main():
     return
