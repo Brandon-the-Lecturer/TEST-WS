42329dfaf2d7097744c793b7d85b30d3f85dcced
Brandon-the-Lecturer <89731991+Brandon-the-Lecturer@users.noreply.github.com>
2024-10-18 20:49:40 +0000

    feat: implement data product type 5

--- src/presentation/dashboard.py
+++ src/presentation/dashboard.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import streamlit as st
 
-import data.play_by_play as DataService_PBP 
+import data.pbp as DataService_PBP 
 import data.drives as DataService_Drives 
 import logic.pbp as LogicService
 
@@ -18,7 +18,6 @@ GAMES = ["KC@BAL", "SF@TEN"]
 
 GAME_STRING_SPLIT_CHAR = "-at-"
 SCOUTING_SIDES = ["Visitors", "Home"]
-SIDE_BAR_BUTTON_NAME = "Bestätigen"
 
 # Function
 
@@ -29,11 +28,11 @@ def get_team_mapping(game_string:str) -> dict:
     teams = game_string.split(GAME_STRING_SPLIT_CHAR)
     return dict(zip(SCOUTING_SIDES, teams))
 
-def get_line_plot_for_points_per_play(team_map: dict, df: pd.DataFrame, display_selection: str):
+def get_line_plot_for_points_per_play(team_map: dict, pbp: pd.DataFrame, display_selection: str):
         
     # Daten in zwei Gruppen aufteilen
-    visitors_possession = df[df['Possession'] == team_map["Visitors"]]
-    home_possessions = df[df['Possession'] == team_map["Home"]]
+    visitors_possession = pbp[pbp['Possession'] == team_map["Visitors"]]
+    home_possessions = pbp[pbp['Possession'] == team_map["Home"]]
     
     visitors_team = team_map["Visitors"].upper()
     home_team = team_map["Home"].upper()
@@ -90,95 +89,106 @@ def get_line_plot_for_points_per_play(team_map: dict, df: pd.DataFrame, display_
 
     return fig
 
+def evaluate_performance(scout_team, team_map, visitors_performance, home_performance):
+    if scout_team == team_map["Visitors"]:
+        team_name = team_map["Visitors"]
+        performance = visitors_performance
+    else:
+        team_name = team_map["Home"]
+        performance = home_performance
+
+    if performance < 100:
+        st.success(f'{team_name} underperforms with {performance}% - No Danger.')
+    else:
+        st.error(f'{team_name} overperforms with {performance}% - Further Scouting!')
+    return 
+
 
 def main():
     # Header
     st.set_page_config(page_title="Game Scout Report", page_icon="🏈")
 
     # Session
-    # Definiere einen eindeutigen Schlüssel für den Button
     button_key = 'sidebar_button_pressed'
+    selection_key = 'selection'
 
-    # Initialisiere den session_state für den Button, falls er noch nicht existiert
     if button_key not in st.session_state:
         st.session_state[button_key] = False
-
+    
+    if selection_key not in st.session_state:
+        st.session_state[selection_key] = dict()
+    
     # Sidebar
     with st.sidebar:
         st.header("GAME SELECTION")
-        
+
         selected_season = st.selectbox(label="Season", options=DataService_PBP.get_seasons())
         selected_week = st.selectbox(label="Week", options=DataService_PBP.get_week_of_season(season=selected_season))
         selected_game = st.selectbox(label="Game", options=DataService_PBP.get_game_of_week(week=selected_week, season=selected_season), format_func=display_game)
         
+        game_string_visitors, game_string_home = selected_game.split(GAME_STRING_SPLIT_CHAR)
 
-        team_map = get_team_mapping(selected_game)
-
-        scout_team = st.radio(label="Team to scout", options=team_map.values())
+        scout_team = st.radio(label="Team to scout", options=[game_string_visitors, game_string_home])
         
         if DataService_Drives.check_for_data(selected_season, selected_week, scout_team):
             st.success("Daten vollständig")
-            if st.button(SIDE_BAR_BUTTON_NAME):
+            
+            if st.button("Bestätigen"):
                 st.session_state[button_key] = True
+                st.session_state[selection_key] = {
+                    "Season": selected_season,
+                    "Week": selected_week,
+                    "Game": selected_game,
+                    "Visitors": game_string_visitors,
+                    "Home": game_string_home,
+                    "Scout": scout_team
+                }
         else: 
             st.error("Daten unvollständig")
     
-    
     if st.session_state[button_key]:
+        
+        selection = st.session_state[selection_key]
         
         # Main Body
         st.header(f"Scout Report for {scout_team.upper()}")
+        
         header_col1, header_col2, header_col3, header_col4 = st.columns(4)
-        header_col1.metric(label="Season", value=selected_season)
-        header_col2.metric(label="Week", value=selected_week)
-        header_col3.metric(label="Visitors", value=team_map["Visitors"].upper())
-        header_col4.metric(label="Home", value=team_map["Home"].upper())
+
+        header_col1.metric(label="Season", value=selection["Season"])
+        header_col2.metric(label="Week", value=selection["Week"])
+        header_col3.metric(label="Visitors", value=selection["Visitors"].upper())
+        header_col4.metric(label="Home", value=selection["Home"].upper())
 
 
         
 
         with st.expander("Data Product Type 1: Raw Data"):            
-            pbp_data = LogicService.get_pbp_data(selected_season, selected_week, selected_game)
+            pbp_data = LogicService.get_pbp_data(selection["Season"], selection["Week"], selection["Game"])
             st.write(pbp_data)
         
         with st.expander("Data Product Type 2: Aggregated Data"):
-            pbp_data_with_pos = LogicService.get_pbp_data_agg(selected_season, selected_week, selected_game)
+            pbp_data_with_pos = LogicService.get_pbp_data_agg(selection["Season"], selection["Week"], selection["Game"])
             st.write(pbp_data_with_pos)
         
         with st.expander("Data Product Type 3: Algorithm"):
-            pbp_data_with_xp = LogicService.get_pbp_data_with_xp(selected_season, selected_week, selected_game)
+            pbp_data_with_xp = LogicService.get_pbp_data_with_xp(selection["Season"], selection["Week"], selection["Game"])
             st.write(pbp_data_with_xp)
         
         with st.expander("Data Product Type 4: Supported Decision"):
-            display_selection = st.radio("Selection", options=[*team_map.values(), "Both"], index=2)
+            display_selection = st.radio("Selection", options=[selection["Visitors"], selection["Home"], "Both"], index=2)
 
-            line_plot = get_line_plot_for_points_per_play(team_map, pbp_data_with_xp, display_selection)
+            line_plot = get_line_plot_for_points_per_play(selection, pbp_data_with_xp, display_selection)
             st.pyplot(line_plot)
         
         with st.expander("Data Product Type 5: Automated Decision"):
 
-            visitors_performance = LogicService.get_team_performance(team=team_map["Visitors"], side="Visitors", data=pbp_data_with_xp)
-            home_performance = LogicService.get_team_performance(team=team_map["Home"], side="Home", data=pbp_data_with_xp)
-
-            # Ergebnisse anzeigen
+            visitors_performance, home_performance = LogicService.get_performances(selection["Season"], selection["Week"], selection["Visitors"], selection["Home"])
             col1, col2 = st.columns(2)
-            col1.metric(label=f"Performance {team_map['Visitors']}", value=f"{visitors_performance}%", delta=f"{visitors_performance - home_performance}%")
-            col2.metric(label=f"Performance {team_map['Home']}", value=f"{home_performance}%", delta=f"{home_performance - visitors_performance}%")
+            col1.metric(label=f"Performance {selection['Visitors']}", value=f"{visitors_performance}%", delta=f"{visitors_performance - home_performance}%")
+            col2.metric(label=f"Performance {selection['Home']}", value=f"{home_performance}%", delta=f"{home_performance - visitors_performance}%")
 
-            if scout_team == team_map["Visitors"]:
-                if visitors_performance < 100:
-                    st.success(f'{team_map["Visitors"]} underperforms with {visitors_performance}% - No Danger.')
-
-                else:
-                    st.error(f'{team_map["Visitors"]} overperforms with {visitors_performance}% - Futher Scouting!')
-
-            
-            else:
-                if visitors_performance < 100:
-                    st.success(f'{team_map["Home"]} underperforms with {home_performance}% - No Danger.')
-
-                else:
-                    st.error(f'{team_map["Home"]} overperforms with {home_performance}% - Futher Scouting!')
+            evaluate_performance(scout_team, selection, visitors_performance, home_performance)
 
 
 
